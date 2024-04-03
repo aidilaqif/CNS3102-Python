85c29cf980a50666d15a6e2436777975be453af0
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-04-04 06:52:37 +0800

    first commit

---
+++ Chapter 1 - Python Structure & Syntax/python_loops_break_statement.py
@@ -0,0 +1,16 @@
+# Exit the loop when i is 3:
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+# Use of break statement inside loop
+
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+print("The end")
