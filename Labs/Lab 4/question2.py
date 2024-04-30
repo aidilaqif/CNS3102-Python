266b9679df4ebb534e425b6b7f3d39b34db372fa
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-04-30 16:48:52 +0800

    Create question2.py

---
+++ Labs/Lab 4/question2.py
@@ -0,0 +1,14 @@
+# CNS3102
+# Python Programming
+# Sem 2 2023/2024
+# Lab 4 Questions : Lists
+
+# Q2: List Slicing
+# • Create a list named fruits containing "apple", "banana", "cherry", "date", "elderberry".
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+# • Print the first three elements of the list.
+print(f"first 3 elements: ", fruits[:3])
+# • Print the last two elements of the list.
+print(f"last 2 elements: ", fruits[-2:])
+# • Print the elements from index 1 to index 3 (inclusive).
+print(f"elements from index 1 to 3: ", fruits[1:4])
